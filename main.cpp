68dd9ba98ab00e22423861d36b10002dbb8b6df4
nandiom84ian <nandiom84ian@kolabnow.com>
2026-02-17 01:58:32 +0700

    Create main core

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+
+int num1 = 10;
+int num2 = 20;
+
+   int sum = num1 + num2;
+   printf ("tong cua chung %d + %d : %d\n", num1, num2, sum );
+    
+   
+  return 0;
+}
